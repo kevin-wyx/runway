adc557aa83a9c92e3214e2f1f532d34153682f72
Gerard Gine <ggine@swiftstack.com>
2017-05-12 16:58:06 -0700

    Properly disabled ProxyFS installation

--- start.sh
+++ start.sh
@@ -53,10 +53,10 @@ $DIR/setup_and_run_ansible_on_guest.sh $CNAME $DEBUG
 # look for the common install script and run it on the container
 
 PROXYFS_INSTALLER=/home/swift/code/ProxyFS/src/github.com/swiftstack/ProxyFS/ci/ansible/install_proxyfs_runway.sh
-if lxc exec $CNAME -- ls $PROXYFS_INSTALLER; then
-    # TODO: Uncomment next line to install ProxyFS!
-    # lxc exec $CNAME -- $PROXYFS_INSTALLER
-fi
+# TODO: Uncomment this code to install ProxyFS
+#if lxc exec $CNAME -- ls $PROXYFS_INSTALLER; then
+    #lxc exec $CNAME -- $PROXYFS_INSTALLER
+#fi
 
 DELETE_CONTAINER=''
 if [[ " $* " =~ " --delete-container " ]]; then

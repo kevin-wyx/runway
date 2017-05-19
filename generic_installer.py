e6bb995c0df6c7ed2460943cd30a2c72e62927ea
Gerard Gine <ggine@swiftstack.com>
2017-05-19 16:24:04 -0700

    Trying to make generic_installer error management better

--- generic_installer.py
+++ generic_installer.py
@@ -15,13 +15,27 @@ container_name = sys.argv[1]
 
 found_repos = []
 for entry in os.scandir('components'):
-    if entry.is_dir() and entry.name not in ('swift', 'python-swiftclient', 'liberasurecode'):
+    if entry.is_dir() and entry.name not in ('swift',
+                                             'python-swiftclient',
+                                             'liberasurecode') and \
+            os.path.isfile('components/{}/install.sh'.format(entry.name)):
         found_repos.append(entry.name)
 
 for repo_path in found_repos:
-    cmd = 'lxc exec %s -- /bin/bash /home/swift/code/%s/install.sh' % (container_name, repo_path)
+    cmd = 'lxc exec {} -- /bin/bash /home/swift/code/{}/install.sh'.format(
+        container_name, repo_path)
     print(cmd)
-    p = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # catch/log errors
-    print('stdout:\n%s' % p.stdout.decode())
-    print('\n\nstderr:\n%s' % p.stderr.decode())
+    try:
+        p = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE, check=True)
+
+        # catch/log errors
+        print('stdout:\n%s' % p.stdout.decode())
+        print('\n\nstderr:\n%s' % p.stderr.decode())
+    except subprocess.CalledProcessError as e:
+        # catch/log errors
+        print('stdout:\n%s' % p.stdout.decode())
+        print('\n\nstderr:\n%s' % p.stderr.decode())
+
+        print(e.output)
+        exit(e.returncode)

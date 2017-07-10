57bbe67988a344c5f62b159158df271cd71907ef
Gerard Gine <ggine@swiftstack.com>
2017-07-10 16:17:53 -0700

    New script: setup_guest_workspace.py
    Updated old build_vm_and_container.py to call setup_guest_workspace.py

--- bin/build_vm_and_container.py
+++ bin/build_vm_and_container.py
@@ -6,13 +6,12 @@ import sys
 
 from runway_utils import colorprint
 from runway_utils.cli import run_command
-from runway_utils.manifest import Manifest
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 RUNWAY_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..'))
-WORKSPACE_DIR = os.path.join(RUNWAY_DIR, 'components')
-DEFAULT_MANIFEST_NAME = 'default_manifest.cfg'
-DEFAULT_MANIFEST_PATH = os.path.join(RUNWAY_DIR, DEFAULT_MANIFEST_NAME)
+BIN_DIR = os.path.abspath(os.path.join(RUNWAY_DIR, 'bin'))
+SETUP_WORKSPACE_SCRIPT = 'setup_guest_workspace.py'
+VAGRANT_BOX_NAME = "ubuntu/xenial64"
 
 
 def exit_on_error(error_text):
@@ -22,52 +21,64 @@ def exit_on_error(error_text):
                                                 'cleanup_runway.sh')))
     sys.exit(1)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', default=DEFAULT_MANIFEST_PATH,
-                        help="Path to manifest file. Default: '/path"
-                             "/to/runway/{}'".format(DEFAULT_MANIFEST_NAME))
+    parser.add_argument('-m', '--manifest', default=None,
+                        help="Path to manifest file. Run '{}/{} -h' to check "
+                             "the default manifest "
+                             "file.".format(BIN_DIR, SETUP_WORKSPACE_SCRIPT))
     parser.add_argument('-u', '--do_not_update_box', action='store_true',
                         default=False, help="Do not try to update Vagrant box")
+    parser.add_argument('-w', '--workspace', default=None,
+                        help="Workspace name")
 
     args = parser.parse_args()
-    config_file = os.path.abspath(args.config)
+    manifest_file = os.path.abspath(args.manifest) \
+        if args.manifest is not None else None
     update_box = not args.do_not_update_box
+    workspace_name = args.workspace
 
-    try:
-        current_manifest = Manifest(config_file, WORKSPACE_DIR)
-        current_manifest.retrieve_components()
-    except Exception as e:
-        exit_on_error(e.message)
+    # Setup workspace
+    cmd = "./setup_guest_workspace.py"
+    if manifest_file is not None:
+        cmd += " -m {}".format(manifest_file)
+    if workspace_name is not None:
+        cmd += " -w {}".format(workspace_name)
+    run_command(cmd, cwd=BIN_DIR)
 
+    # Update Vagrant box
     if update_box:
-        colorprint.info("We will keep updating the box until we get a stable "
-                   "release...")
+        colorprint.info("\nWe will keep updating the box until we get a "
+                        "stable release...")
         try:
-            run_command("vagrant box update --box ubuntu/xenial64",
+            run_command("vagrant box update --box {}".format(VAGRANT_BOX_NAME),
                         cwd=RUNWAY_DIR)
         except Exception as e:
             exit_on_error(e.message)
         colorprint.info("But we will keep our disks free of old boxes. ;)")
         try:
-            run_command("vagrant box prune --name ubuntu/xenial64",
+            run_command("vagrant box prune --name {}".format(VAGRANT_BOX_NAME),
                         cwd=RUNWAY_DIR)
         except Exception as e:
             exit_on_error(e.message)
     else:
         colorprint.info("Skipping box update")
 
+    # Vagrant up
     if os.environ.get('CONTROLLER_NAME') is None:
-        colorprint.warning("WARNING: CONTROLLER_NAME env var hasn't been set. If "
-                      "you fail to 'vagrant up' your VM, open VirtualBox, "
-                      "check the name of your SCSI Controller and provide it "
-                      "in the CONTROLLER_NAME env var.")
+        colorprint.warning("WARNING: CONTROLLER_NAME env var hasn't been set. "
+                           "If you fail to 'vagrant up' your VM, open "
+                           "VirtualBox, check the name of your SCSI "
+                           "Controller and provide it in the CONTROLLER_NAME "
+                           "env var.")
     try:
         run_command("vagrant up", cwd=RUNWAY_DIR)
     except Exception as e:
         exit_on_error(e.message)
 
     # Log into our brand new container?
-    colorprint.success("Your new container is now up and running! If you want to "
-                  "log into it, just run the following command from the "
-                  "runway directory:\n\n\tbin/bash_on_current_container.sh")
+    colorprint.success("Your new container is now up and running! If you want "
+                       "to log into it, just run the following command from "
+                       "the runway directory:\n\n\t"
+                       "bin/bash_on_current_container.sh")
---
+++ bin/setup_guest_workspace.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import argparse
+import glob
+import os
+import sys
+
+from runway_utils import colorprint
+from runway_utils.manifest import Manifest
+from shutil import copyfile
+
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+RUNWAY_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..'))
+WORKSPACE_DIR = os.path.join(RUNWAY_DIR, 'guest_workspaces')
+WORKSPACE_PREFIX = 'runway-'
+DEFAULT_MANIFEST_NAME = 'default_manifest.cfg'
+DEFAULT_MANIFEST_PATH = os.path.join(RUNWAY_DIR, DEFAULT_MANIFEST_NAME)
+MANIFEST_COPY_NAME = 'manifest.cfg'
+MAXIMUM_WORKSPACE_COUNT = 999
+
+
+def exit_on_error(error_text):
+    colorprint.error(error_text)
+    colorprint.error("\nIf you want to cleanup your runway installation, run "
+                     "'{}'".format(os.path.join(RUNWAY_DIR, 'bin',
+                                                'cleanup_runway.sh')))
+    sys.exit(1)
+
+
+def get_maximum_workspace_index_length():
+    return len(str(MAXIMUM_WORKSPACE_COUNT))
+
+
+def get_last_workspace_index():
+    wildcard_expression = os.path.join(WORKSPACE_DIR,
+                                       '{}'.format(WORKSPACE_PREFIX))
+    for i in xrange(get_maximum_workspace_index_length()):
+        wildcard_expression += "[0-9]"
+    current_workspaces = sorted([os.path.basename(name) for name in
+                                 glob.glob(wildcard_expression)], reverse=True)
+    if len(current_workspaces) == 0:
+        return None
+    last_index = int(current_workspaces[0][len(WORKSPACE_PREFIX):])
+    return last_index
+
+
+def get_new_workspace_name():
+    last_index = get_last_workspace_index()
+    if last_index is None:
+        new_index = 1
+    elif last_index < MAXIMUM_WORKSPACE_COUNT:
+        new_index = last_index + 1
+    else:
+        raise Exception("You have reached the maximum workspace index "
+                        "({}).".format(MAXIMUM_WORKSPACE_COUNT))
+    return "{}{:03d}".format(WORKSPACE_PREFIX, new_index)
+
+
+def create_workspace_dir(workspace_name=None):
+    if workspace_name is None:
+        # Get name for our new workspace
+        user_provided_workspace = False
+        try:
+            workspace_name = get_new_workspace_name()
+        except Exception as e:
+            exit_on_error(e.message)
+    else:
+        user_provided_workspace = True
+
+    colorprint.info("\nRetrieving components into workspace '{}'..."
+                    "\n".format(workspace_name))
+    new_workspace_path = os.path.join(WORKSPACE_DIR, workspace_name)
+    try:
+        os.mkdir(new_workspace_path)
+    except OSError as e:
+        # If the user provided the workspace name, he might be trying to update
+        # it, so it's fine if we get error 17 (File exists).
+        if not user_provided_workspace or e.errno != 17:
+            exit_on_error("Could not create directory '{}': [Errno {}] "
+                          "{}".format(new_workspace_path, e.errno, e.strerror))
+
+    return new_workspace_path
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--manifest', default=DEFAULT_MANIFEST_PATH,
+                        help="Path to manifest file. Default: '/path"
+                             "/to/runway/{}'".format(DEFAULT_MANIFEST_NAME))
+    parser.add_argument('-w', '--workspace', default=None,
+                        help="Workspace name")
+
+    args = parser.parse_args()
+    manifest_file = os.path.abspath(args.manifest)
+    workspace_name = args.workspace
+
+    # Create new workspace directory
+    new_workspace_path = create_workspace_dir(workspace_name)
+
+    # Copy manifest into workspace
+    copyfile(manifest_file, os.path.join(new_workspace_path,
+                                         MANIFEST_COPY_NAME))
+
+    # Retrieve contents
+    try:
+        current_manifest = Manifest(manifest_file, new_workspace_path)
+        current_manifest.retrieve_components()
+    except Exception as e:
+        exit_on_error(e.message)
+
+    colorprint.success("Guest workspace successfully created at "
+                       "'{}'.".format(new_workspace_path))

846bbef2621ea2731c82261713a1ce685743648b
Gerard Gine <ggine@swiftstack.com>
2017-08-03 21:01:17 -0700

    Now we have to hit 'return' after 'y' to confirm cleaning up runway

--- bin/cleanup_runway.sh
+++ bin/cleanup_runway.sh
@@ -7,11 +7,11 @@ cd $SCRIPTDIR/..
 eval RUNWAYDIR=`pwd`
 
 echo
-# If we want the user to have to hit 'return' key, we just have to remove
-# the '-n 1' part.
 echo "Running this script will delete everything under '${RUNWAYDIR}/components'"
 echo "and everything under '${RUNWAYDIR}/guest_workspaces', and will destroy"
-read -p "your Vagrant VM. Are you sure you want to continue? [y/N] " -n 1 -r
+read -p "your Vagrant VM. Are you sure you want to continue? [y/N] " -r
+# If want the user to hit 'return' key, replace previous line with next one:
+#read -p "your Vagrant VM. Are you sure you want to continue? [y/N] " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "Aborting cleanup"

7bf58e5998ef1dc690461df2e126751d96d23077
Gerard Gine <ggine@swiftstack.com>
2017-09-13 19:50:20 -0700

    Updated warning message on cleanup_runway.sh to be more scary

--- bin/cleanup_runway.sh
+++ bin/cleanup_runway.sh
@@ -5,11 +5,22 @@ set -e
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTDIR/..
 eval RUNWAYDIR=`pwd`
+WORKSPACES_PATH=$RUNWAYDIR/guest_workspaces
 
 echo
-echo "Running this script will delete everything under '${RUNWAYDIR}/components'"
-echo "and everything under '${RUNWAYDIR}/guest_workspaces', and will destroy"
-read -p "your Vagrant VM. Are you sure you want to continue? [y/N] " -r
+echo "                                                _____  "
+echo "*******************************************    /     \ "
+echo "*                                         *   | () () |"
+echo "*     WARNING! Your work can be lost!     *    \  ^  / "
+echo "*                                         *     |||||  "
+echo "*******************************************     |||||  "
+echo
+echo "Running this script will delete everything under '${WORKSPACES_PATH}',"
+echo "and will destroy your Vagrant VM along with all the containers it hosts."
+echo
+echo "All your uncommitted work in Runway WILL BE LOST. FOREVER."
+echo
+read -p "Are you sure you want to continue? [y/N] " -r
 # If want the user to hit 'return' key, replace previous line with next one:
 #read -p "your Vagrant VM. Are you sure you want to continue? [y/N] " -n 1 -r
 echo
@@ -20,5 +31,5 @@ fi
 
 cd $SCRIPTDIR
 vagrant destroy --force
-cd $RUNWAYDIR/guest_workspaces
+cd $WORKSPACES_PATH
 ls | grep -v README | xargs rm -rf

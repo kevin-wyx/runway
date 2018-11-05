d6c0412308201e46e4845634b69e1cf2e82f8191
craig harmer <charmer@swiftstack.com>
2018-11-05 14:35:21 -0800

    Fixes so create_continer runs under Linux.
    Change lib/cli.py so it handles UTF-8 output from commands better.
    
    This changes the way lines are parsed so that `decode('utf-8')` is used
    only when running under Python 3.

--- bin/create_container.sh
+++ bin/create_container.sh
@@ -5,6 +5,7 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPTDIR/lib/get_container_connection_options.sh
 
+
 # Ideally we should get expected options from the manifest
 # But for now, you must specify -d <DISTRO> -v <VOLSIZE> e.g., -d RHEL -v 1G
 ssh ${VAGRANTOPTIONS} ${RUNWAYHOST} RUNWAYCNAME=${RUNWAYCNAME} sudo /vagrant/start.py ${*} && echo "Done"
